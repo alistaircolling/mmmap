9cd019d3826470b027f8b7448c1380bc5db2d06f
alistair colling <alistair.colling@me.com>
2010-04-16 01:07:37 +0100

    added new sql dump

---
+++ mmmap_2010-04-16.sql
@@ -0,0 +1,159 @@
+﻿# Sequel Pro dump
+# Version 1630
+# http://code.google.com/p/sequel-pro
+#
+# Host: 127.0.0.1 (MySQL 5.0.41)
+# Database: mmmap
+# Generation Time: 2010-04-16 01:06:37 +0100
+# ************************************************************
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table customers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `customers`;
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` (`id`,`name`)
+VALUES
+	(1,'customer1name'),
+	(2,'customer2name'),
+	(3,'customer3name');
+
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table preferences
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `preferences`;
+
+CREATE TABLE `preferences` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) default NULL,
+  `value` text,
+  `user_id` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `preferences` WRITE;
+/*!40000 ALTER TABLE `preferences` DISABLE KEYS */;
+INSERT INTO `preferences` (`id`,`name`,`value`,`user_id`)
+VALUES
+	(1,'defaultsearchparameters','{\"customer\":1, \"product\":1, \"fromDate\":\"\", \"toDate\":\"\",\"saleType\":\"rentals\"}',1),
+	(2,'defaultsearchparameters','{\"customer\":2, \"product\":2, \"fromDate\":\"2010-01-01\", \"toDate\":\"2010-01-31\",\"saleType\":\"sales\"}',2),
+	(3,'defaultsearchparameters','{\"customer\":3, \"product\":3, \"fromDate\":\"2010-02-01\", \"toDate\":\"2010-03-28\",\"saleType\":\"all\"}',3);
+
+/*!40000 ALTER TABLE `preferences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) default NULL,
+  `img_name` varchar(40) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` (`id`,`name`,`img_name`)
+VALUES
+	(1,'Stop Sign','images/stopSign.jpg'),
+	(2,'Go Sign','images/goSign.jpg'),
+	(3,'Falling Rocks','images/fallingRocks.jpg'),
+	(4,'Roadworks','images/roadWorks.jpg'),
+	(5,'Skiddy Roads','images/skid.jpg'),
+	(6,'Lollipop Sign','images/skid.jpg');
+
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table transactions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `transactions`;
+
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL auto_increment,
+  `customer_id` int(11) default NULL,
+  `product_id` int(11) default NULL,
+  `sale_date` date default NULL,
+  `type` varchar(10) default NULL,
+  `rental_start` date default NULL,
+  `rental_end` date default NULL,
+  `created_by` int(11) default NULL,
+  `position` varchar(40) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` (`id`,`customer_id`,`product_id`,`sale_date`,`type`,`rental_start`,`rental_end`,`created_by`,`position`)
+VALUES
+	(1,1,1,'2010-04-01','sale',NULL,NULL,1,'54.57373994324158, -6.82324218750001'),
+	(2,2,3,NULL,'rental','2010-04-03','2010-04-05',2,'53.64635407782998, -1.5058593750000093'),
+	(3,2,2,'2010-01-01','sale',NULL,NULL,1,'57.68220791347688, -4.625976562500009'),
+	(4,3,4,NULL,'rental','2010-01-01','2010-01-31',3,'51.70159424562466, -0.09960937500000933,');
+
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(20) default NULL,
+  `password` varchar(255) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`,`username`,`password`)
+VALUES
+	(1,'alidrongo','5f4dcc3b5aa765d61d8327deb882cf99'),
+	(2,'mikos','5f4dcc3b5aa765d61d8327deb882cf99'),
+	(3,'user3','5f4dcc3b5aa765d61d8327deb882cf99');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
